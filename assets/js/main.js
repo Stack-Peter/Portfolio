9ee622121bc65a9ca770b473bd140828f54bc335
Pedro Marcos Ferreira <stack.peter21@gmail.com>
2023-08-13 21:06:07 -0300

    13/08/2023 updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,6 @@ function scrollActive() {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id')
-
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         } else {
